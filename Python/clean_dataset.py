362f66778980a1c19818897b556ff19b668bd60a
Lina Heinzke <heinzke@ebi.ac.uk>
2023-08-05 12:49:19 +0100

    Improve documentation for clean_dataset

--- Python/clean_dataset.py
+++ Python/clean_dataset.py
@@ -1,36 +1,47 @@
 import pandas as pd
+import sqlite3
 
-def remove_irrelevant_compounds(df_combined, chembl_con):
-    # TODO: REFACTOR
-    # Remove Compounds Without a Smiles and Mixtures
-    # Remove Compounds With a Smiles Containing a '.'
-    # Double-check that rows with a SMILES containing a '.' are the parent structures, 
-    # i.e., there was no error in using salt information instead of parent information.  
-    # These compounds are salts or mixtures and will be removed in the next step.
+
+def remove_compounds_without_smiles_and_mixtures(df_combined: pd.DataFrame, chembl_con: sqlite3.Connection) -> pd.DataFrame:
+    """
+    Remove 
+
+    - compounds without a smiles 
+    - compounds with smiles containing a dot (mixtures and salts).
+
+    Since compound information is aggregated for the parents of salts, 
+    the number of smiles with a dot is relatively low.
+
+    :param df_combined: Pandas DataFrame with compound-target pairs
+    :type df_combined: pd.DataFrame
+    :param chembl_con: Sqlite3 connection to ChEMBL database.
+    :type chembl_con: sqlite3.Connection
+    :return: Pandas DataFrame with compound-target pairs with a smiles that does not contain a '.'
+    :rtype: pd.DataFrame
+    """
+    # Double-check that rows with a SMILES containing a '.' are the parent structures,
+    # i.e., there was no error in using salt information instead of parent information.
     sql = '''
     SELECT DISTINCT mh.molregno as salt_molregno, mh.parent_molregno
     FROM molecule_hierarchy mh
     '''
     df_hierarchy = pd.read_sql_query(sql, con=chembl_con)
 
-    smiles_with_dot = df_combined[df_combined['canonical_smiles'].notnull() & df_combined['canonical_smiles'].str.contains('.', regex=False)]
-    smiles_with_dot = smiles_with_dot[['canonical_smiles', 'parent_molregno']].drop_duplicates()
+    smiles_with_dot = df_combined[df_combined['canonical_smiles'].notnull(
+    ) & df_combined['canonical_smiles'].str.contains('.', regex=False)][[
+        'canonical_smiles', 'parent_molregno']].drop_duplicates()
 
-    issue_ctr = 0
     for parent_molregno in set(smiles_with_dot['parent_molregno']):
-        # the molrego should occur at least once as a parent_molregno
-        if not len(df_hierarchy[df_hierarchy['parent_molregno'] == parent_molregno]) > 0:
-            print(df_hierarchy[df_hierarchy['parent_molregno'] == parent_molregno])
-            issue_ctr += 1
-        # if it occurs as a salt_molregno, its parent_molregno should be identical, 
-        # i.e., the molregno is a parent_molregno
-        df_salt_molregno = df_hierarchy[df_hierarchy['salt_molregno'] == parent_molregno]
-        if not df_salt_molregno['salt_molregno'].equals(df_salt_molregno['parent_molregno']):
-            print(df_hierarchy[df_hierarchy['salt_molregno'] == parent_molregno])
-            issue_ctr += 1
+        assert (len(df_hierarchy[df_hierarchy['parent_molregno'] == parent_molregno]) > 0), \
+            f"The compound with the parent_moregno {parent_molregno} does not occur \
+                as a parent_molregno in the molecule_hierarchy."
+        df_salt_molregno = df_hierarchy[df_hierarchy['salt_molregno']
+                                        == parent_molregno]
+        assert (df_salt_molregno['salt_molregno'].equals(df_salt_molregno['parent_molregno'])), \
+            f"The compound with the parent_moregno {parent_molregno} occurs as a salt \
+                but has a different parent in the molecule_hierarchy."
 
-    print("#Problems:", issue_ctr)
-
+    # Double-check that the SMILES is indeed the SMILES for the parent structure.
     sql = '''
     SELECT DISTINCT mh.parent_molregno, struct.canonical_smiles
     FROM molecule_hierarchy mh
@@ -39,33 +50,31 @@ def remove_irrelevant_compounds(df_combined, chembl_con):
     '''
     df_parent_smiles = pd.read_sql_query(sql, con=chembl_con)
 
-    issue_ctr = 0
     for parent_molregno in set(smiles_with_dot['parent_molregno']):
-        df_smiles = df_parent_smiles[df_parent_smiles['parent_molregno'] == parent_molregno]['canonical_smiles'].item()
-        smiles_w_dot = smiles_with_dot[smiles_with_dot['parent_molregno'] == parent_molregno]['canonical_smiles'].item()
-        if df_smiles != smiles_w_dot:
-            print(parent_molregno)
-            print(df_smiles)
-            print(smiles_w_dot)
-            print()
-            issue_ctr += 1
-
-    print("#Problems:", issue_ctr)
+        parent_smiles_in_chembl = df_parent_smiles[df_parent_smiles['parent_molregno']
+                                                   == parent_molregno]['canonical_smiles'].item()
+        parent_smiles_in_df = smiles_with_dot[smiles_with_dot['parent_molregno']
+                                              == parent_molregno]['canonical_smiles'].item()
+        assert (parent_smiles_in_chembl == parent_smiles_in_df), \
+            f"The smiles for the compound {parent_molregno} ({parent_smiles_in_df}) \
+                in the dataframe is not the same as \
+                the smiles for the compound in ChEMBL ({parent_smiles_in_chembl})."
 
     # Remove rows that contain a SMILES with a dot or that don't have a SMILES.
-    len_missing_smiles = len(df_combined[df_combined['canonical_smiles'].isnull()])
-    len_smiles_w_dot = len(df_combined[df_combined['parent_molregno'].isin(set(smiles_with_dot['parent_molregno']))])
-    print("{:30} {}".format("#Rows w/o SMILES:", len_missing_smiles))
-    print("{:30} {}".format("#Rows w SMILES with dot:", len_smiles_w_dot))
-    print("{:30} {}".format("Predicted size after removal:", len(df_combined)-len_missing_smiles-len_smiles_w_dot))
-    df_combined = df_combined[(df_combined['canonical_smiles'].notnull()) & ~(df_combined['parent_molregno'].isin(set(smiles_with_dot['parent_molregno'])))]
-    print("{:30} {}".format("Size:", len(df_combined)))
+    # # TODO log this information in debugging mode
+    # len_missing_smiles = len(df_combined[df_combined['canonical_smiles'].isnull()])
+    # len_smiles_w_dot = len(df_combined[df_combined['parent_molregno'].isin(set(smiles_with_dot['parent_molregno']))])
+
+    df_combined = df_combined[(df_combined['canonical_smiles'].notnull()) & ~(
+        df_combined['parent_molregno'].isin(set(smiles_with_dot['parent_molregno'])))]
 
     return df_combined
 
 
 def clean_none_values(df_combined):
-    # Change nan values and empty strings to None for consistency. 
+    """
+    Change nan values and empty strings to None for consistency.
+    """
     # Change all None / nan values to None
     df_combined = df_combined.where(pd.notnull(df_combined), None)
     # replace empty strings with None
@@ -73,8 +82,11 @@ def clean_none_values(df_combined):
 
     return df_combined
 
-def set_types(df_combined, calculate_RDKit):
-    # Set relevant columns to int
+
+def set_types_to_int(df_combined, calculate_RDKit):
+    """
+    Set the type of relevant columns to Int64.
+    """
     df_combined = df_combined.astype({
         'first_approval': 'Int64',
         'usan_year': 'Int64',
@@ -116,80 +128,101 @@ def set_types(df_combined, calculate_RDKit):
 
     return df_combined
 
-def round_floats(df_combined, decimal_places = 4):
-    # Round float columns to <decimal_places> decimal places
-    for i, (col, dtype) in enumerate(df_combined.dtypes.to_dict().items()):
+
+def round_floats(df_combined, decimal_places=4):
+    """
+    Round float columns to <decimal_places> decimal places.
+    This does not apply to max_phase.
+    """
+    for _, (col, dtype) in enumerate(df_combined.dtypes.to_dict().items()):
         if ((dtype == 'float64') or (dtype == 'Float64')) and col != 'max_phase':
             df_combined[col] = df_combined[col].round(decimals=decimal_places)
-    
+
     return df_combined
+
 
 def reorder_columns(df_combined, calculate_RDKit):
+    """
+    Reorder the columns in the DataFrame.
+    """
     len_columns_before = len(df_combined.columns)
+
+    compound_target_pair_columns = ['parent_molregno', 'parent_chemblid', 'parent_pref_name',
+                                    'max_phase', 'first_approval', 'usan_year', 'black_box_warning',
+                                    'prodrug', 'oral', 'parenteral', 'topical',
+                                    'tid', 'mutation', 'target_chembl_id', 'target_pref_name', 'target_type',
+                                    'organism', 'tid_mutation',
+                                    'cpd_target_pair', 'cpd_target_pair_mutation']
+    aggregated_values = ['pchembl_value_mean_BF', 'pchembl_value_max_BF', 'pchembl_value_median_BF',
+                         'first_publication_cpd_target_pair_BF', 'first_publication_cpd_target_pair_w_pchembl_BF',
+                         'pchembl_value_mean_B', 'pchembl_value_max_B', 'pchembl_value_median_B',
+                         'first_publication_cpd_target_pair_B', 'first_publication_cpd_target_pair_w_pchembl_B']
+    DTI_annotations = ['therapeutic_target', 'DTI']
+    first_publication_cpd = ['first_publication_cpd']
+    chembl_compound_props = ['mw_freebase', 'alogp', 'hba', 'hbd', 'psa',
+                             'rtb', 'ro3_pass', 'num_ro5_violations', 'cx_most_apka', 'cx_most_bpka',
+                             'cx_logp', 'cx_logd', 'molecular_species', 'full_mwt', 'aromatic_rings',
+                             'heavy_atoms', 'qed_weighted', 'mw_monoisotopic', 'full_molformula',
+                             'hba_lipinski', 'hbd_lipinski', 'num_lipinski_ro5_violations']
+    chembl_structures = ['standard_inchi',
+                         'standard_inchi_key', 'canonical_smiles']
+    ligand_efficieny_metrics = ['LE_B', 'BEI_B', 'SEI_B', 'LLE_B',
+                                'LE_BF', 'BEI_BF', 'SEI_BF', 'LLE_BF']
+    chembl_target_annotations = ['atc_level1',
+                                 'target_class_l1', 'target_class_l2']
+    rdkit_columns = ['fraction_csp3',
+                     'num_aliphatic_carbocycles', 'num_aliphatic_heterocycles', 'num_aliphatic_rings',
+                     'num_aromatic_carbocycles', 'num_aromatic_heterocycles', 'num_aromatic_rings',
+                     'num_heteroatoms',
+                     'num_saturated_carbocycles', 'num_saturated_heterocycles', 'num_saturated_rings',
+                     'ring_count', 'num_stereocentres',
+                     'aromatic_atoms', 'aromatic_c', 'aromatic_n', 'aromatic_hetero',
+                     'scaffold_w_stereo', 'scaffold_wo_stereo']
+    filtering_columns = ['in_dm_table', 'keep_for_binding']
+
     if calculate_RDKit:
-        df_combined = df_combined[['parent_molregno', 'parent_chemblid', 'parent_pref_name', 
-                                'max_phase', 'first_approval', 'usan_year', 'black_box_warning', 
-                                'prodrug', 'oral', 'parenteral', 'topical',
-                                'tid', 'mutation', 'target_chembl_id', 'target_pref_name', 'target_type', 
-                                'organism', 'tid_mutation',
-                                'cpd_target_pair', 'cpd_target_pair_mutation',
-                                'pchembl_value_mean_BF', 'pchembl_value_max_BF', 'pchembl_value_median_BF',
-                                'first_publication_cpd_target_pair_BF', 'first_publication_cpd_target_pair_w_pchembl_BF',
-                                'pchembl_value_mean_B', 'pchembl_value_max_B', 'pchembl_value_median_B',
-                                'first_publication_cpd_target_pair_B', 'first_publication_cpd_target_pair_w_pchembl_B', 
-                                'therapeutic_target', 'DTI',
-                                'first_publication_cpd', 'mw_freebase', 'alogp', 'hba', 'hbd', 'psa',
-                                'rtb', 'ro3_pass', 'num_ro5_violations', 'cx_most_apka', 'cx_most_bpka',
-                                'cx_logp', 'cx_logd', 'molecular_species', 'full_mwt', 'aromatic_rings',
-                                'heavy_atoms', 'qed_weighted', 'mw_monoisotopic', 'full_molformula',
-                                'hba_lipinski', 'hbd_lipinski', 'num_lipinski_ro5_violations', 
-                                'standard_inchi', 'standard_inchi_key', 'canonical_smiles', 
-                                'LE_B', 'BEI_B', 'SEI_B', 'LLE_B',
-                                'LE_BF', 'BEI_BF', 'SEI_BF', 'LLE_BF',
-                                'atc_level1', 'target_class_l1', 'target_class_l2', 
-                                'fraction_csp3', 
-                                'num_aliphatic_carbocycles', 'num_aliphatic_heterocycles', 'num_aliphatic_rings', 
-                                'num_aromatic_carbocycles', 'num_aromatic_heterocycles', 'num_aromatic_rings',
-                                'num_heteroatoms', 
-                                'num_saturated_carbocycles', 'num_saturated_heterocycles', 'num_saturated_rings', 
-                                'ring_count', 'num_stereocentres', 
-                                'aromatic_atoms', 'aromatic_c', 'aromatic_n', 'aromatic_hetero', 
-                                'scaffold_w_stereo', 'scaffold_wo_stereo',
-                                'in_dm_table', 'keep_for_binding']]
+        columns = compound_target_pair_columns + \
+            aggregated_values + DTI_annotations + first_publication_cpd + \
+            chembl_compound_props + chembl_structures + \
+            ligand_efficieny_metrics + \
+            chembl_target_annotations + \
+            rdkit_columns + \
+            filtering_columns
+        df_combined = df_combined[columns]
     else:
-        df_combined = df_combined[['parent_molregno', 'parent_chemblid', 'parent_pref_name', 
-                                'max_phase', 'first_approval', 'usan_year', 'black_box_warning', 
-                                'prodrug', 'oral', 'parenteral', 'topical',
-                                'tid', 'mutation', 'target_chembl_id', 'target_pref_name', 'target_type', 
-                                'organism', 'tid_mutation',
-                                'cpd_target_pair', 'cpd_target_pair_mutation',
-                                'pchembl_value_mean_BF', 'pchembl_value_max_BF', 'pchembl_value_median_BF',
-                                'first_publication_cpd_target_pair_BF', 'first_publication_cpd_target_pair_w_pchembl_BF',
-                                'pchembl_value_mean_B', 'pchembl_value_max_B', 'pchembl_value_median_B',
-                                'first_publication_cpd_target_pair_B', 'first_publication_cpd_target_pair_w_pchembl_B', 
-                                'therapeutic_target', 'DTI',
-                                'first_publication_cpd', 'mw_freebase', 'alogp', 'hba', 'hbd', 'psa',
-                                'rtb', 'ro3_pass', 'num_ro5_violations', 'cx_most_apka', 'cx_most_bpka',
-                                'cx_logp', 'cx_logd', 'molecular_species', 'full_mwt', 'aromatic_rings',
-                                'heavy_atoms', 'qed_weighted', 'mw_monoisotopic', 'full_molformula',
-                                'hba_lipinski', 'hbd_lipinski', 'num_lipinski_ro5_violations', 
-                                'standard_inchi', 'standard_inchi_key', 'canonical_smiles', 
-                                'LE_B', 'BEI_B', 'SEI_B', 'LLE_B',
-                                'LE_BF', 'BEI_BF', 'SEI_BF', 'LLE_BF',
-                                'atc_level1', 'target_class_l1', 'target_class_l2', 
-                                'in_dm_table', 'keep_for_binding']]
+        columns = compound_target_pair_columns + \
+            aggregated_values + DTI_annotations + first_publication_cpd + \
+            chembl_compound_props + chembl_structures + \
+            ligand_efficieny_metrics + \
+            chembl_target_annotations + \
+            filtering_columns
+        df_combined = df_combined[columns]
 
     len_columns_after = len(df_combined.columns)
-    assert(len_columns_before == len_columns_after), "Different number of columns after reordering."
+    assert (len_columns_before ==
+            len_columns_after), f"Different number of columns after reordering (before: {len_columns_before}, after: {len_columns_after})."
 
     return df_combined
 
-def clean_dataset(df_combined, chembl_con, calculate_RDKit):
+
+def clean_dataset(df_combined: pd.DataFrame, calculate_RDKit: bool) -> pd.DataFrame:
+    """
+    Clean the dataset by 
+
+    - changing nan values and empty strings to None
+    - setting the type of relevant columns to Int64
+    - rounding floats to 4 decimal places (with the exception of max_phase which is not rounded)
+    - reordering columns
+
+    :param df_combined: Pandas DataFrame with compound-target pairs
+    :type df_combined: pd.DataFrame
+    :param calculate_RDKit: True if the DataFrame contains RDKit-based compound properties
+    :type calculate_RDKit: bool
+    :return: Cleaned pandas DataFrame with compound-target pairs
+    :rtype: pd.DataFrame
+    """
     df_combined = clean_none_values(df_combined)
-    df_combined = set_types(df_combined, calculate_RDKit)
-    df_combined = round_floats(df_combined, decimal_places = 4)
+    df_combined = set_types_to_int(df_combined, calculate_RDKit)
+    df_combined = round_floats(df_combined, decimal_places=4)
     df_combined = reorder_columns(df_combined, calculate_RDKit)
     return df_combined
-
-
-
